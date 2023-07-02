3fe932fc273578a6f04008f82b91690fc0690079
Stella Mazzillo <stellagress5@gmail.com>
2023-07-02 12:00:10 -0400

    adds a search tool in ParticipatingCountries.js

--- src/components/ParticipatingCountries.js
+++ src/components/ParticipatingCountries.js
@@ -1,26 +1,45 @@
 
 
-
-import React from "react";
+import React, { useState } from "react";
 import data from "../data.json";
 
+
 function ParticipatingCountries() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCountries = data.countries.filter((country) =>
+    country.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div>
+    <div className="countries-container">
       <h2>Women's World Cup 2023 - List of 32 teams:</h2>
-      {data.countries.map((country, index) => (
-        <div key={index}>
-          <img src={country.flag} alt={country.name} />
-          {country.name}
-        </div>
-      ))}
+      <h4>check if your country is participating:</h4>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search country"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="countries-grid">
+        {filteredCountries.map((country, index) => (
+          <div className="country-item" key={index}>
+            <img src={country.flag} alt={country.name} />
+            <strong>{country.name}</strong>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default ParticipatingCountries;
-
-
 
 
 
