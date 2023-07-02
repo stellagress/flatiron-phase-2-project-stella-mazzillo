8a977e82918290dc3c4ad7fb5fc695433301c613
Stella Mazzillo <stellagress5@gmail.com>
2023-07-02 14:27:10 -0400

    adds comments to NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,9 +2,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+
+//The NavBar component displays a navigation bar with links to 3 different pages of the application.
+
 function NavBar(){
     return (
         <div className="navbar">
+            {/* Link#1 - Home link */}
             <NavLink
             to="/"
             exact
@@ -14,7 +18,7 @@ function NavBar(){
                 Home
             </NavLink>
 
-
+            {/* Link#2 - Participating Countries link */}
             <NavLink
             to="/participating-countries"
             exact
@@ -25,6 +29,7 @@ function NavBar(){
             </NavLink>
 
 
+            {/* Link#3 - Games link */}
             <NavLink
             to="/games"
             exact
