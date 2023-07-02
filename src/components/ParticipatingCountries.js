58b59c9a423397973541a53ebb7709e9be4a5944
Stella Mazzillo <stellagress5@gmail.com>
2023-07-02 11:39:31 -0400

    adds flags to ParticipatingCountries.js

--- src/components/ParticipatingCountries.js
+++ src/components/ParticipatingCountries.js
@@ -1,24 +1,45 @@
 
 
 
-
-
-
-
 import React from "react";
 import data from "../data.json";
 
-
 function ParticipatingCountries() {
-    return (
-        <div>
-            <h2>Women's World Cup 2023 - List of 32 teams:</h2>
-            {data.countries.map((country, index) => (
-                <div key={index}>{country}</div>
-            ))}
+  return (
+    <div>
+      <h2>Women's World Cup 2023 - List of 32 teams:</h2>
+      {data.countries.map((country, index) => (
+        <div key={index}>
+          <img src={country.flag} alt={country.name} />
+          {country.name}
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-
 export default ParticipatingCountries;
+
+
+
+
+
+
+
+// import React from "react";
+// import data from "../data.json";
+
+
+// function ParticipatingCountries() {
+//     return (
+//         <div>
+//             <h2>Women's World Cup 2023 - List of 32 teams:</h2>
+//             {data.countries.map((country, index) => (
+//                 <div key={index}>{country}</div>
+//             ))}
+//         </div>
+//     )
+// }
+
+
+// export default ParticipatingCountries;
