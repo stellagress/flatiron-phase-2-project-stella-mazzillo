6d4832ad838ed92643e232a7c4bfc4b53988443f
Stella Mazzillo <stellagress5@gmail.com>
2023-07-02 18:40:42 -0400

    cleans up .js files

--- src/components/ParticipatingCountries.js
+++ src/components/ParticipatingCountries.js
@@ -54,23 +54,3 @@ function ParticipatingCountries() {
 export default ParticipatingCountries;
 
 
-
-
-
-// import React from "react";
-// import data from "../data.json";
-
-
-// function ParticipatingCountries() {
-//     return (
-//         <div>
-//             <h2>Women's World Cup 2023 - List of 32 teams:</h2>
-//             {data.countries.map((country, index) => (
-//                 <div key={index}>{country}</div>
-//             ))}
-//         </div>
-//     )
-// }
-
-
-// export default ParticipatingCountries;
