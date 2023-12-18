58d6e731f02cee9a28f65447f7e0680c54baab30
Stella Mazzillo <stellagress5@gmail.com>
2023-12-18 00:59:09 -0500

    updates server

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// // Serve static files from the public directory
-// app.use(express.static('public'));
+// Serve static files from the public directory
+app.use(express.static('public'));
 
-// // Rest of your server code
-// const jsonServer = require('json-server')
-// const router = jsonServer.router('db.json')
-// const middlewares = jsonServer.defaults()
-// app.use(middlewares);
-// app.use('/api', router);
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
-
+// Rest of your server code
 const jsonServer = require('json-server')
-
-const server = jsonServer.create()
-
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+app.use(middlewares);
+app.use('/api', router);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+// const jsonServer = require('json-server')
+
+// const server = jsonServer.create()
+
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
  
-server.use(middlewares)
-server.use('/api', router)
-server.listen(process.env.PORT || 5000, () => {
-  console.log('JSON Server is running')
-})
+// server.use(middlewares)
+// server.use('/api', router)
+// server.listen(process.env.PORT || 5000, () => {
+//   console.log('JSON Server is running')
+// })
