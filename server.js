f6ff6cb3f6b7e59e2322de55c71f12dbf6c26862
Stella Mazzillo <stellagress5@gmail.com>
2023-12-28 13:46:59 -0500

    tries express server.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-// Serve static files from the public directory
-app.use(express.static('public'));
+// // Serve static files from the public directory
+// app.use(express.static('public'));
 
-// Rest of your server code
-const jsonServer = require('json-server')
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-// app.use(middlewares);
+// // Rest of your server code
+// const jsonServer = require('json-server')
+// const router = jsonServer.router('db.json')
+// const middlewares = jsonServer.defaults()
+// // app.use(middlewares);
+// // app.use('/api', router);
+// app.use('/api', middlewares);
 // app.use('/api', router);
-app.use('/api', middlewares);
-app.use('/api', router);
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// // Start the server
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+// ----------------------------------------
+
 
 
 // const jsonServer = require('json-server')
@@ -32,3 +37,26 @@ app.listen(PORT, () => {
 // server.listen(process.env.PORT || 5000, () => {
 //   console.log('JSON Server is running')
 // })
+
+
+
+
+
+const express = require('express');
+const app = express();
+
+// Serve static files from the public directory
+app.use(express.static('public'));
+
+// Rest of your server code
+const jsonServer = require('json-server')
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+app.use(middlewares);
+app.use('/api', router);
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
